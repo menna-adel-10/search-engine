bda148d4b59e2d3ef2f18e5cede48af7f9b52097
menna-adel-10 <menna10_adel@hotmail.com>
2024-04-03 15:23:08 +0200

    .

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Icons } from "@/components/icons";
+import SearchBar from "@/components/SearchBar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -42,9 +44,19 @@ aria-hidden='true'>
           
           <div className="mx-auto max-w-7xl px-6 pb-24 pt-10 sm:pb-32 lg:flex gap-16 lg:px-8 lg:py-24">
             <div className="h-full w-full flex flex-col items-center gap-4">
+              <Icons.Sparkles className="w-16 h-16"/>
+              
               <h1 className="tracking-tight text-4xl sm:text-6xl font-bold">
                 MagicSearch
               </h1>
+              <p className="max-w-xl text-center text-lg text-slate-700">
+                Hybrid Search Engine that enhances search accuracy by querying semantically related results.
+              </p>
+
+              <div className="mx-auto mt-16 w-full max-w-2xl flex flex-col">
+                <SearchBar />
+                   {children}
+              </div>
             </div>
           </div>
         </div>
